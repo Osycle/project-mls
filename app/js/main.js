d5497d4ad91409dcc5458c52753beb3a123378f3
osycle <osycle@gmail.com>
2017-10-29 21:07:12 +0500

    29.10

--- app/js/main.js
+++ app/js/main.js
@@ -22,6 +22,16 @@ $(function(){
 		setTimeout(function(){$(el).removeClass("invisible")}, 600);
 		setTimeout(function(){$(el).removeClass("aos-animate")}, 100);
 	})
+	$(".p-animated ul li").map(function(i, el){
+		$(el).attr({
+					"data-aos": "fade-up",
+					"data-aos-duration": 600,
+					"data-aos-delay": 100*i});
+		$(el).addClass("invisible");
+		setTimeout(function(){$(el).removeClass("invisible")}, 600);
+		setTimeout(function(){$(el).removeClass("aos-animate")}, 100);
+	});
+
 	// AOS
 	AOS.init({
 	  offset: 0,
@@ -32,53 +42,18 @@ $(function(){
 	setTimeout(function(){AOS.refresh()}, 300);
 
 
-var i = {
-	// GOOGLE MAP INIT
-	gm_content: '<h2>DAEWOO ALTOEN</h2>'+
-					'<p>Адрес: МО., г. Долгопрудный, Промышленный проезд 14, офис 314</p>'+
-					'<p>Телефон: +7(495)922-3765</p>'+
-					'<p>E-mail: info@altoendaewoo.ru</p>',
-	gm_title : 'Мы',
-	gm_icon: 					"img/icons/marker.png",
-
-	gm_marker_position_lat:  41.296781,
-	gm_marker_position_lng:  69.226306,
-
-	gm_position_lat: 			 41.2968543,
-	gm_position_lng: 			 69.2080361
-};
-
-
-
-
 var header_status = false;
 
 
-window.$.fn.initMenu = function(option){
-
-	var options = $.extend({
-		"menuToggleBtn"					: false, 		// Кнопка бара
-		"subMenu"								: false, 		// Класс подменю
-		"modalMenu"							: false, 		// Модальное меню
-
-		"menuToggle"						: Function, 	// Переключение
-		"menuHoverIn"						: Function,
-		"menuHoverOut"					: Function,
-		"subHoverIn"						: Function,
-		"subHoverOut"						: Function,
-		"modalMenuShow"					: Function, 	// Открытие меню
-		"modalMenuShown"				: Function,		// Меню раскрыт
-		"modalMenuHide"					: Function, 	// Раскрытие Меню
-		"modalMenuHidden"				: Function 		// Меню скрыт
 
 
-	}, option );
-	var menu = new Menu(this, options);
-
-	return menu;
-
-}
-
+	//menu init
+	$(".nav-menu").initMenu({
+		"menuToggleBtn": ".menu-toggle",
+		"subMenu": ".sub-menu-1",
+		"modalMenu": "#menuModal",
+		menuHoverIn: function(){}
+	})
 
 
 function phoneDap(){
@@ -88,6 +63,17 @@ function phoneDap(){
 		.before( $(".social") )
 }
 
+function dropbtn(){
+	$(".dropdown-lang .dropbtn").on("click", function(){
+		$( $(this).siblings(".dropdown-content") ).toggleClass("active");
+	});
+	$(".dropdown-lang .dropdown-content").on("mouseleave", function(){
+		$(this).removeClass("active");
+	});
+}
+dropbtn();
+
+
 
 //RESIZE
 $( window ).on("resize", function(e){
@@ -96,23 +82,98 @@ $( window ).on("resize", function(e){
 
 });
 //SCROLL
-$( window ).on("scroll", function(){
-	if($(window).scrollTop() > 500 && header_status == false){
+$( window ).on("scroll", function(e){
+	
+	if($(window).scrollTop() > 300 && header_status == false){
 		
 
-		$("#header").addClass("navbar-scroll");
+		//$(".header-scroll").addClass("in");
 		
 		header_status = true; 
 
-	}else if($(window).scrollTop() < 500 && header_status == true){
+	}else if($(window).scrollTop() < 300 && header_status == true){
 
-		$("#header").removeClass("navbar-scroll");
+		//$(".header-scroll").removeClass("in");
 		
 		header_status = false;
 
 	}
 
 });
+
+
+
+
+
+//COMMON FUNTIONS
+
+function sendForm(th){
+
+	this.onsubmit = function(e){ e.preventDefault();}
+	var require = $(th).serialize();
+	send(require+"&to="+to);
+
+	$(th).find("input").val("");
+}
+
+function ajPost(u, d, s, c){
+	$.ajax({
+		type: 		"POST",
+		url: 			u,
+		data: 		d,
+		success: 	s,
+		statusCode: {
+			404: function(){alert("Страница не найдена");}
+		},
+		complete: c
+	});
+}
+ $.fn.fadeToggleBool = function( dura = 290 ){
+ 	var self = $( $(this) ),
+ 		 bool = self.css("display") == "none";
+
+	self.fadeToggle({
+
+		duration: dura,
+		easing: "linear"
+
+	});
+
+	return bool;
+ }
+
+function modalShadow( el ){
+
+	if( $(modal_shadow).length == 0 && el.jquery) 
+		return;
+
+	if( modal_shadow.fadeToggleBool() ){
+		modal_shadow.on("click", function(){
+			if(el.length != 0)
+				el.trigger("click");
+			});
+	}else
+		modal_shadow.off("click");
+}
+
+function scrolledDiv(el) {
+	try{
+	  var docViewTop = $(window).scrollTop(),
+		docViewBottom = docViewTop + $(window).height(),
+		elTop = $(el).offset().top,
+		elBottom = elTop + $(el).height()/1.8;
+	}catch(err){console.error();}
+
+  	return ((elBottom <= docViewBottom) && (elTop >= docViewTop));
+}
+
+
+	});//$
+}) (jQuery);
+
+
+
+
 
 
 
@@ -211,71 +272,35 @@ function Menu( menu, options ){
 
 
 
-//COMMON FUNTIONS
 
-function sendForm(th){
+window.$.fn.initMenu = function(option){
 
-	this.onsubmit = function(e){ e.preventDefault();}
-	var require = $(th).serialize();
-	send(require+"&to="+to);
+	var options = $.extend({
+		"menuToggleBtn"					: false, 		// Кнопка бара
+		"subMenu"								: false, 		// Класс подменю
+		"modalMenu"							: false, 		// Модальное меню
 
-	$(th).find("input").val("");
-}
+		"menuToggle"						: Function, 	// Переключение
+		"menuHoverIn"						: Function,
+		"menuHoverOut"					: Function,
+		"subHoverIn"						: Function,
+		"subHoverOut"						: Function,
+		"modalMenuShow"					: Function, 	// Открытие меню
+		"modalMenuShown"				: Function,		// Меню раскрыт
+		"modalMenuHide"					: Function, 	// Раскрытие Меню
+		"modalMenuHidden"				: Function 		// Меню скрыт
 
-function ajPost(u, d, s, c){
-	$.ajax({
-		type: 		"POST",
-		url: 			u,
-		data: 		d,
-		success: 	s,
-		statusCode: {
-			404: function(){alert("Страница не найдена");}
-		},
-		complete: c
-	});
-}
- $.fn.fadeToggleBool = function( dura = 290 ){
- 	var self = $( $(this) ),
- 		 bool = self.css("display") == "none";
 
-	self.fadeToggle({
+	}, option );
+	var menu = new Menu(this, options);
 
-		duration: dura,
-		easing: "linear"
+	return menu;
 
-	});
-
-	return bool;
- }
-
-function modalShadow( el ){
-
-	if( $(modal_shadow).length == 0 && el.jquery) 
-		return;
-
-	if( modal_shadow.fadeToggleBool() ){
-		modal_shadow.on("click", function(){
-			if(el.length != 0)
-				el.trigger("click");
-			});
-	}else
-		modal_shadow.off("click");
-}
-
-function scrolledDiv(el) {
-	try{
-	  var docViewTop = $(window).scrollTop(),
-		docViewBottom = docViewTop + $(window).height(),
-		elTop = $(el).offset().top,
-		elBottom = elTop + $(el).height()/1.8;
-	}catch(err){console.error();}
-
-  	return ((elBottom <= docViewBottom) && (elTop >= docViewTop));
 }
 
 
-	});//$
-}) (jQuery);
+
+
 
 
 
